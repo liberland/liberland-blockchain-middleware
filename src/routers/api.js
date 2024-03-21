7dc187b192cc8d3ccf3847029d21c59e9a8cf513
Szymon.Zieba <szymon.zieba@neti-soft.com>
2024-03-21 10:10:20 +0100

    Remove prettier fix

--- src/routers/api.js
+++ src/routers/api.js
@@ -4,9 +4,9 @@ const router = require("express").Router();
 const wrap = require("express-async-handler");
 const { ApiPromise, WsProvider, Keyring } = require("@polkadot/api");
 const config = require("../../config");
-const axios = require("axios");
-const { BN } = require("@polkadot/util");
-const generateCertificate = require("./generate-certificate");
+const axios =require ('axios');
+const {BN} = require("@polkadot/util")
+const generateCertificate = require('./generate-certificate')
 
 const provider = new WsProvider(config.RPC_NODE_URL);
 const apiPromise = ApiPromise.create({
@@ -185,141 +185,97 @@ router.post(
 			baseURL: config.CENTRALIZED_API_URL,
 			withCredentials: false,
 		});
-		centralizedAPI.defaults.headers.common["X-token"] = userToken;
+		centralizedAPI.defaults.headers.common['X-token'] = userToken;
 
-		let centralizedCheckPromises = [];
+		let centralizedCheckPromises = []
 
 		let checkEligibleForPersonIdPromise = new Promise((resolve, reject) => {
-			centralizedAPI
-				.get("/e-residents/approved/me")
-				.then((result) => {
-					if (result.status !== 200) {
-						return reject(
-							"You are not a fully approved e-resident or citizen, therefore are not eligible to claim onboarding LLDs"
-						);
-					}
-					if (result.data.claimedOnboardingLld !== false) {
-						return reject("Person already claimed onboarding LLD");
-					}
-					centralizedId = result.data.id;
-					return resolve(centralizedId);
-				})
-				.catch((e) => {
-					return reject(
-						"Technical error when checking if fully approved e-resident who didnt claim LLD yet, please let the devs know"
-					);
-				});
-		});
+			centralizedAPI.get('/e-residents/approved/me').then((result) => {
+				if(result.status !== 200) {
+					return reject('You are not a fully approved e-resident or citizen, therefore are not eligible to claim onboarding LLDs');
+				}
+				if(result.data.claimedOnboardingLld !== false) {
+					return reject('Person already claimed onboarding LLD');
+				}
+				centralizedId = result.data.id;
+				return resolve(centralizedId)
+			}).catch(e =>{
+				return reject('Technical error when checking if fully approved e-resident who didnt claim LLD yet, please let the devs know');
+			})
 
-		centralizedCheckPromises.push(checkEligibleForPersonIdPromise);
+		})
+
+		centralizedCheckPromises.push(checkEligibleForPersonIdPromise)
 
 		let checkWalletAddressPromise = new Promise((resolve, reject) => {
-			centralizedAPI
-				.get("/users/me")
-				.then((result) => {
-					centralizedWalletAddress = result.data.blockchainAddress;
-					if (centralizedWalletAddress !== usingWalletAddress) {
-						return reject(
-							"Wallet address provided and wallet address registered in profile do not match"
-						);
-					}
-					return resolve();
-				})
-				.catch((e) => {
-					return reject(
-						"Technical error when checking if used and registered wallet address match, please let the devs know"
-					);
-				});
-		});
+			centralizedAPI.get('/users/me').then((result) => {
+				centralizedWalletAddress = result.data.blockchainAddress;
+				if(centralizedWalletAddress !== usingWalletAddress) {
+					return reject('Wallet address provided and wallet address registered in profile do not match');
+				}
+				return resolve()
+			}).catch(e => {
+				return reject('Technical error when checking if used and registered wallet address match, please let the devs know');
+			});
+		})
 
-		centralizedCheckPromises.push(checkWalletAddressPromise);
+		centralizedCheckPromises.push(checkWalletAddressPromise)
 
 		let checkIfAlreadyHaveLLDPromise = new Promise((resolve, reject) => {
-			api.derive.balances
-				.all(usingWalletAddress)
-				.then((result) => {
-					const availableBalance = new BN(result.availableBalance);
-					if (!availableBalance.isZero()) {
-						return reject(
-							"User not eligible as account has LLDs already"
-						);
-					}
-					return resolve(availableBalance);
-				})
-				.catch((e) => {
-					console.log(e);
-					return reject(
-						"Technical error when checking wallet LLDs, please let the devs know"
-					);
-				});
-		});
-
-		centralizedCheckPromises.push(checkIfAlreadyHaveLLDPromise);
-
-		return Promise.all(centralizedCheckPromises)
-			.then((results) => {
-				const keyring = new Keyring({ type: "sr25519" });
-				const sender = keyring.addFromMnemonic(config.ONBOARDER_PHRASE);
-				const sendExtrinsic = api.tx.balances.transfer(
-					usingWalletAddress,
-					2000000000000
-				);
-
-				let sendLLDPromise = new Promise((resolve, reject) =>
-					sendExtrinsic.signAndSend(
-						sender,
-						({ events = [], status }) => {
-							if (status.isInBlock) {
-								const err = events.find(({ event }) =>
-									api.events.system.ExtrinsicFailed.is(event)
-								);
-								if (err) {
-									if (err.event.data[0].isModule) {
-										const decoded =
-											api.registry.findMetaError(
-												err.event.data[0].asModule
-											);
-										const { docs, method, section } =
-											decoded;
-										reject({ docs, method, section });
-									} else {
-										reject(err.toString());
-									}
-								} else {
-									resolve(events);
-								}
-							}
-						}
-					)
-				);
-
-				sendLLDPromise
-					.then((result) => {
-						centralizedAPI
-							.patch(
-								"/e-residents/applications/" + centralizedId,
-								{ claimedOnboardingLld: true }
-							)
-							.then((result) => {
-								return res.status(200).send("All went well");
-							})
-							.catch((e) => {
-								console.log("cought e");
-								console.log(e);
-								return res.status(401).send(e);
-							});
-					})
-					.catch((e) => {
-						console.log("cought e");
-						console.log(e);
-						return res.status(401).send(e);
-					});
+			api.derive.balances.all(usingWalletAddress).then(result => {
+				const availableBalance = new BN(result.availableBalance)
+				if(!availableBalance.isZero()) {
+					return reject('User not eligible as account has LLDs already');
+				}
+				return resolve(availableBalance)
+			}).catch(e => {
+				console.log(e)
+				return reject('Technical error when checking wallet LLDs, please let the devs know');
 			})
-			.catch((e) => {
-				console.log("cought e");
-				console.log(e);
-				return res.status(401).send(e);
-			});
+		})
+
+		centralizedCheckPromises.push(checkIfAlreadyHaveLLDPromise)
+
+		return Promise.all(centralizedCheckPromises).then(results => {
+			const keyring = new Keyring({ type: 'sr25519' });
+			const sender = keyring.addFromMnemonic(config.ONBOARDER_PHRASE)
+			const sendExtrinsic = api.tx.balances.transfer(usingWalletAddress, (2000000000000))
+
+			let sendLLDPromise = new Promise((resolve, reject) => sendExtrinsic.signAndSend(sender, ({ events = [], status }) => {
+				if (status.isInBlock) {
+					const err = events.find(({ event }) => api.events.system.ExtrinsicFailed.is(event))
+					if (err) {
+						if (err.event.data[0].isModule) {
+							const decoded = api.registry.findMetaError(err.event.data[0].asModule);
+							const { docs, method, section } = decoded;
+							reject({ docs, method, section })
+						} else {
+							reject(err.toString())
+						}
+					} else {
+						resolve(events)
+					}
+				}
+			}));
+
+			sendLLDPromise.then(result => {
+				centralizedAPI.patch('/e-residents/applications/' + centralizedId,  { claimedOnboardingLld: true }).then((result) => {
+					return res.status(200).send('All went well')
+				}).catch(e => {
+					console.log('cought e')
+					console.log(e)
+					return res.status(401).send(e)
+				})
+			}).catch(e => {
+				console.log('cought e')
+				console.log(e)
+				return res.status(401).send(e)
+			})
+		}).catch(e => {
+			console.log('cought e')
+			console.log(e)
+			return res.status(401).send(e)
+		})
 	})
 );
 

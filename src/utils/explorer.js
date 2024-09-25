3fcc02ae7eceb1c2ec99b6e82bd20812fa8bd659
Kacper Żuk <kacper.zuk@neti-soft.com>
2024-09-25 14:17:11 +0200

    LLD Stats endpoint

---
+++ src/utils/explorer.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const axios = require("axios");
+const config = require("../../config");
+
+const eraPaidEventsQuery = `
+query EraPaidEvents {
+  events(
+    orderBy: BLOCK_NUMBER_DESC,
+    first: 28,
+    filter: {
+      method: { equalTo: "EraPaid" },
+      section: { equalTo: "staking" }
+    }
+  ) {
+    nodes {
+      data
+    }
+  }
+}
+`;
+
+const getApi = () => axios.create({
+  baseURL: config.EXPLORER_GRAPHQL,
+});
+
+
+const getLastWeekEraPaidEvents = async () => {
+  const { data } = await getApi().post('', {
+    query: eraPaidEventsQuery
+  });
+  return data.data.events.nodes.map(v => v.data);
+};
+
+module.exports = {
+  getLastWeekEraPaidEvents,
+}
